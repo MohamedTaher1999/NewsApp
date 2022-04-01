017336672b7f0680683ef8673e164e144f2dc6fe
Taher <mohamed22.taher22@gmail.com>
2022-04-01 16:16:32 +0200

    Setup Retrofit

---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY="728076a714c14502889f0cafc960c4d2"
+        const val BASE_URL="https://newsapi.org"
+    }
+}
